8025c6aa889701e4bc624f6c9e0e37d29b515b65
co2ma <67058333+co2ma@users.noreply.github.com>
2020-09-11 11:47:26 +0900

    [코드 수정]

--- 04_example_3.ino
+++ 04_example_3.ino
@@ -14,11 +14,12 @@ void setup() {
 
 void loop() {
   Serial.println(++count);
-  toggle = toggle_state(count%2); //toggle LED value.
+  toggle = toggle_state(toggle); //toggle LED value.
   digitalWrite(PIN_LED, toggle); // update LED status.
   delay(1000); // wait for 1,000 milliseconds
 }
 
 int toggle_state(int toggle) {
-  return toggle;   
+  toggle++;
+  return toggle%2;   
 }
